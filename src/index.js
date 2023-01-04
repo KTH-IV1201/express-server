54fb6699937afbe0976c38649668181cb8675472
Leif <leifl@kth.se>
2023-01-04 09:39:30 +0100

    change url for cors

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ app.get('/', (req, res) => {
   res.send({result: 'No CORS enabled here'});
 });
 app.get('/cors', (req, res) => {
-  res.set('Access-Control-Allow-Origin', 'https://iv1201-cors-client.herokuapp.com/');
+  res.set('Access-Control-Allow-Origin', 'https://iv1201-cors-client.herokuapp.com');
   // res.set('Access-Control-Allow-Origin', '*');
   res.set('Access-Control-Allow-Methods', '*');
   res.set('Access-Control-Allow-Headers', '*');
