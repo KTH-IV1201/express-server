34cedb13203903858d016d199c71114d8b097049
Leif <leifl@kth.se>
2023-01-04 09:42:50 +0100

    cleaned

--- src/index.js
+++ src/index.js
@@ -7,9 +7,6 @@ app.get('/', (req, res) => {
 });
 app.get('/cors', (req, res) => {
   res.set('Access-Control-Allow-Origin', 'https://iv1201-cors-client.herokuapp.com');
-  // res.set('Access-Control-Allow-Origin', '*');
-  res.set('Access-Control-Allow-Methods', '*');
-  res.set('Access-Control-Allow-Headers', '*');
   res.send({result: 'This has CORS enabled'});
 });
 app.listen(PORT, () => {
